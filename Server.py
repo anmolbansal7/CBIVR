6eaefc3216a8399200cdf9ce71ca39822bdc636b
NikhilKP631197 <60618728+NikhilKP631197@users.noreply.github.com>
2021-05-26 12:14:38 +0530

    URL navigation done, Final Push

--- Server.py
+++ Server.py
@@ -25,20 +25,32 @@ def index():
         query = df.extract(img)
         dists = np.linalg.norm(features-query, axis=1)
         ids = np.argsort(dists)[:5]
-        scores = [(dists[id], img_paths[id]) for id in ids]
+        scores = [(img_paths[id].stem, img_paths[id]) for id in ids]
 
         return render_template('index.html', query_path=uploaded_img_path, scores=scores)
     else:
         return render_template('index.html')
     
-"""@app.route('/login', methods=['POST'])
+@app.route('/upload', methods=['GET','POST'])
 def upload():
     if request.method == 'POST':
-       file = request.files['query_img']
+        file = request.files['query_img']
         img = Image.open(file.stream)
-        uploaded_img_path = "static/uploaded/" + datetime.now().isoformat().replace(":", ".") + "_" + file.filename
+        room = request.form['room']
+        store = request.form['store']
+        rack = request.form['rack']
+        name = room + '-' + store + '-' + rack
+        uploaded_img_path = "static/image/" + name + ".jpg"
         img.save(uploaded_img_path)
-"""
+        
+        feature = df.extract(img)
+        fpath = Path("./static/feature")/(name + ".npy")
+        np.save(fpath, feature)
+        features.append(feature)
+        
+        return render_template('upload.html')
+    else:
+        return render_template('upload.html')
 
 if __name__=="__main__":
     app.run(host = "127.0.0.1", port = 8000)
