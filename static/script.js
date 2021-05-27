888ff0856426b8ed50e66d575c47cd46407761ca
anmolbansal7 <abanmolbansal5@gmail.com>
2021-05-27 11:04:40 +0530

    ready for db update

--- static/script.js
+++
@@ -1,8 +0,0 @@
-function loginChng(){
-    window.location.href="login.html";
-};
-
-function showResults(){
-    document.getElementById("search").style.display = "none";
-    document.getElementById("results").style.display = "block";
-};
